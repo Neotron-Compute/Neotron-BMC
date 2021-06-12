3b4e7180efd65d471a2882f15bdaac62c5127a1e
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-06-12 15:03:36 +0100

    Set up peripherals.
    Need to amend HAL to support SPI Peripheral mode (it is Controller
    only at the moment).

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 /// This is the build-script for the Neotron BMC.
 ///
-/// It just copies the memory.x file somewhere Cargo can fine it.
+/// It just copies the memory.x file somewhere Cargo can find it, then generates a version header.
 use std::env;
 use std::fs::File;
 use std::io::Write;
@@ -15,4 +15,14 @@ fn main() {
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
     println!("cargo:rerun-if-changed=memory.x");
+
+
+    let version_output = std::process::Command::new("git")
+    .current_dir(env::var_os("CARGO_MANIFEST_DIR").unwrap())
+    .args(&["describe", "--tags", "--all", "--dirty"])
+    .output()
+    .expect("running git-describe");
+    assert!(version_output.status.success());
+
+    std::fs::write(out.join("version.txt"), version_output.stdout).expect("writing version file");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@ use core::sync::atomic::{AtomicUsize, Ordering};
 
 use defmt_rtt as _; // global logger
 use stm32f0xx_hal as _; // memory layout
-
-use panic_probe as _;
+use panic_probe as _; // panic handler
 
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
 // this prevents the panic message being printed *twice* when `defmt::panic` is invoked
